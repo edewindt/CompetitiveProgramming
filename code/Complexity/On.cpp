3ad709fc55a938b763102fbe31d771896e1cd36d
Elias De Windt <edewindt27@outlook.com>
2023-05-02 10:33:29 -0700

    added O(n) complexity

---
+++ code/Complexity/On.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[] = {1, 2, 3, 4, 5};
+
+int main()
+{
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    
+    return 0;
+}
