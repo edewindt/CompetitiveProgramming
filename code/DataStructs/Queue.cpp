6bd78b17d11329a1761c7b3afa9e878747ea7760
Elias De Windt <edewindt27@outlook.com>
2023-05-02 11:40:57 -0700

    updated Queue

--- code/DataStructs/Queue.cpp
+++ code/DataStructs/Queue.cpp
@@ -8,8 +8,12 @@ int main()
     q.push("First in line");
     q.push("2nd in line");
     q.push("Third in line");
-    cout << q.front() << "\n";
+    cout << q.front() << endl;//looking at the front of the line
+    q.pop(); //front is leaving the line
+    cout << q.front() << endl;
     q.pop();
-    cout << q.front() << "\n";
+    cout << q.front() << endl;
+    q.pop();
+    cout << q.size() << endl;//should be 0
 
 }
