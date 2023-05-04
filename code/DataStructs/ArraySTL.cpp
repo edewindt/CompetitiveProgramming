9cb73e81b536cb13fb742204bc8ecedac1aaae20
Elias De Windt <edewindt27@outlook.com>
2023-05-04 02:36:44 -0700

    added the array class data structure to code

---
+++ code/DataStructs/ArraySTL.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+array<int, 6> arr = {1,2,3,4,5,6};
+int main()
+{
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
