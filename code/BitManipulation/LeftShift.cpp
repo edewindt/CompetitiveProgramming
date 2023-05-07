e8bd1721390fad753c2321938a87f314b7400170
Elias De Windt <edewindt27@outlook.com>
2023-05-07 08:45:08 -0700

    added bit manipulation

---
+++ code/BitManipulation/LeftShift.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << (5<<2) << endl; //5 * 2^2 result:20
+    cout << (5<<10) << endl;// 5 * 2^10 result:5120
+    cout << (10<<5) << endl;// 10 * 2^5 result:320
+    return 0;
+}
---
+++ code/BitManipulation/RightShift.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << (10>>4) << endl; // 10/2^4 result: 0
+    cout << (60>>2) << endl; // 60/2^4 result: 15
+    cout << (160>>4) << endl; // 160/2^4 result:10
+    return 0;
+}
