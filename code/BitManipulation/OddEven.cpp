6cc2ec696a4bfb0114c2c79376ac0db11a237f5b
Elias De Windt <edewindt27@outlook.com>
2023-05-07 20:21:52 -0700

    added oddeven example to bit manipulation

---
+++ code/BitManipulation/OddEven.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    if (x&1) //checks the last bit with OR operator
+    {
+        cout << "Odd" << endl;
+    }else
+    {
+        cout << "Even" << endl;
+    }
+    
+    
+    return 0;
+}
