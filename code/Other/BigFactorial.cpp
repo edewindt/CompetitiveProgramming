0138b01ca1f5c693b88adc0af19ef2a22047053a
Elias De Windt <edewindt27@outlook.com>
2023-05-21 20:38:33 -0700

    in process of adding BigFactorial algorithm

---
+++ code/Other/BigFactorial.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+void multiply(vector<int> &a, int no, int &size)
+{
+    int carry = 0;
+    for(int i=0; i < size; i++)
+    {
+        int product = a[i] * no + carry;
+        a[i] = product % 10;
+        carry = product / 1;
+    }
+    while (carry)
+    {
+        a[size] = carry % 10;
+        carry = carry/10;
+        size = size + 1;
+    }
+    
+}
+void bigFactorial(int n)
+{
+    vector<int> a(1000,0);
+    a[0] = 1;
+    int size = 1;
+
+    for (int i = 0; i <= n; i++)
+    {
+        multiply(a,i,size);
+    }
+    for (int i = size - 1; i >= 0; i--)
+    {
+       cout << a[i];
+    }
+    cout <<endl;
+    
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    bigFactorial(n);
+    return 0;
+}
