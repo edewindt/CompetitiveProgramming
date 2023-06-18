d2b613914594c67beb80990c179270a50e20a127
Elias De Windt <edewindt27@outlook.com>
2023-06-17 21:24:13 -0700

    added more pointer experiments

--- PlayingWithPointers/Pointers.cpp
+++ PlayingWithPointers/Pointers.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int main()
 {
+    int array1[6] = {1,2,3,4,5,6};
+    cout << *array1 << endl;
+    cout << (*array1 + 1) << endl;
+    char hello[18] = "Hello World";
+    cout << hello << endl;
     int number = 69;
     int *Pointer1 = &number;
     number++;
