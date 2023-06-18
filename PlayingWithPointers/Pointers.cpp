a8580232cc2da97e5050a5e91b1b7ceeff983e4a
Elias De Windt <edewindt27@outlook.com>
2023-06-17 21:39:22 -0700

    added a bit more pointers

--- PlayingWithPointers/Pointers.cpp
+++ PlayingWithPointers/Pointers.cpp
@@ -5,6 +5,15 @@ int main()
     int array1[6] = {1,2,3,4,5,6};
     cout << *array1 << endl;
     cout << (*array1 + 1) << endl;
+    int *array2;
+    array2 = (int*)malloc(5*sizeof(int));
+    array2[0] = 23;
+    cout << *array2 << endl;
+    free(array2);
+    int *array3;
+    array3 = new int[5];
+    array3[0] = 69;
+    cout << *array3 << endl;
     char hello[18] = "Hello World";
     cout << hello << endl;
     int number = 69;
