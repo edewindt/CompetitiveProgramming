c60f145242fd1fe90de3bd1e02a4b1cc0f5fa175
Elias De Windt <edewindt27@outlook.com>
2023-06-20 12:10:33 -0700

    modular exponentiation added

---
+++ code/BitManipulation/ModularExpontiation.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+const int mod = 1e9 + 7;
+
+int power(int a, int b)
+{
+    int res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a; res %= mod;
+        a *= a; a %= mod;
+        b /= 2;
+    }
+    return res;
+}
+
+int32_t main()
+{
+    cout << power(2,10) << endl;
+    return 0;
+}
