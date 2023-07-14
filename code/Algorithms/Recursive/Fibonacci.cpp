fde04507ca610c61e3c4372bd4fe2b1b10f738da
Elias De Windt <edewindt27@outlook.com>
2023-07-14 11:56:11 -0700

    edited fib function

--- code/Algorithms/Recursive/Fibonacci.cpp
+++ code/Algorithms/Recursive/Fibonacci.cpp
@@ -6,10 +6,8 @@ int fib(int n){
     if(n<=1){
         return n;
     }
-
-    int f1 = fib(n-1);
-    int f2 = fib(n-2);
-    return f1 + f2;
+    //recursive case
+    return fib(n-1) + fib(n-2);
 }
 
 int main()
